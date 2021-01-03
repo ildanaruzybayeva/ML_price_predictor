28ebe7f64b0371c9d218fe2526c07ecc152308b8
Ildana <ildana.ruzybayeva@gmail.com>
2021-01-03 16:12:38 +0000

    Regression with vanilla JS

--- Regressions/index.js
+++ Regressions/index.js
@@ -1,13 +1,18 @@
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
+const LinearRegression = require("./linear-regression");
 
-let { features, labels, testFeatures, testLabels } = loadCSV(
-  "./cars.csv",
-  {
-    shuffle: true,
-    splitTest: 50,
-    dataColumns: ["horsepower"],
-    labelColumns: ["mpg"],
-  }
-);
+let { features, labels, testFeatures, testLabels } = loadCSV("./cars.csv", {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ["horsepower"],
+  labelColumns: ["mpg"]
+});
 
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.0001,
+  iterations: 100
+});
+
+regression.train();
+console.log("Updated m is:", regression.m, "Updated b is: ", regression.b);
